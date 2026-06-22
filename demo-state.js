80b3e7cc864ad5631c29eab614fb0bab6f38df0b
PARK SUNGMIN <jpsm0305@naver.com>
2026-06-23 08:40:23 +0900

    센서 상승과 정상 복귀 전환 보정

--- demo-state.js
+++ demo-state.js
@@ -1,5 +1,5 @@
 (function () {
-  const STORAGE_KEY = 'aquaguard-demo-state-v4';
+  const STORAGE_KEY = 'aquaguard-demo-state-v5';
   const CHANNEL_NAME = 'aquaguard-demo-channel';
 
   const scenarios = {
